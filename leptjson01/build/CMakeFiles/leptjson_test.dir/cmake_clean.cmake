6d0fce8c8bfdeee74ed88d665a4809d4fc76ec36
jay <jaysecret@foxmail.com>
2024-05-03 03:26:07 +0800

    finished leptjson 01

---
+++ leptjson01/build/CMakeFiles/leptjson_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptjson_test.dir/test.c.o"
+  "CMakeFiles/leptjson_test.dir/test.c.o.d"
+  "leptjson_test"
+  "leptjson_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leptjson_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ leptjson01/build/CMakeFiles/leptjson_test.dir/test.c.o.d
@@ -0,0 +1,12 @@
+CMakeFiles/leptjson_test.dir/test.c.o: /home/leptjson/leptjson01/test.c \
+ /usr/include/stdc-predef.h /home/leptjson/leptjson01/leptjson.h \
+ /usr/include/stdio.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/local/gcc-6.3.0/lib/gcc/x86_64-pc-linux-gnu/6.3.0/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/local/gcc-6.3.0/lib/gcc/x86_64-pc-linux-gnu/6.3.0/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/stdlib.h /usr/include/bits/stdlib-float.h \
+ /usr/include/string.h
