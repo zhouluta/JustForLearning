df1e678b5b4ab4ae876dfe7f4b1742cd3790c997
jay <jaysecret@foxmail.com>
2024-05-03 06:04:35 +0800

    remove ./leptjson01/build/ directory.

--- leptjson01/build/CMakeFiles/leptjson.dir/leptjson.c.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/leptjson.dir/leptjson.c.o: \
- /home/leptjson/leptjson01/leptjson.c /usr/include/stdc-predef.h \
- /home/leptjson/leptjson01/leptjson.h /usr/include/assert.h \
- /usr/include/features.h /usr/include/sys/cdefs.h \
- /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
- /usr/include/gnu/stubs-64.h /usr/include/stdlib.h \
- /usr/local/gcc-6.3.0/lib/gcc/x86_64-pc-linux-gnu/6.3.0/include/stddef.h \
- /usr/include/bits/stdlib-float.h
